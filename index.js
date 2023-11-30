b30c506227b50b1f0825368e8fb251aaa4136e5a
KaatClinckemaillie <kaat.clinckemaillie@student.howest.be>
2023-11-30 23:47:42 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const app = express()
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const port = process.env.PORT || 80;
+const port = 3000
 
-app.use(express.static('public'));
-
+app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
